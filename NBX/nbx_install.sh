3a46a7fb3c40f42936d07c0d8476365cf9c694a0
Myki <hujialou@1nth.com>
2018-07-17 14:02:57 +0800

    nbx_201807171402

---
+++ NBX/nbx_install.sh
@@ -0,0 +1,15 @@
+SQL连接密码
+localhost
+root
+pa5sw0rd
+nbx
+#backup
+rm -rf /usr/sbin/nbxserv
+#把phpmyadmin.php上传到/NBX/www
+#清空 cfg_lic 表
+
+
+#升级
+./force.sh && ./localinstall.sh && ./update.sh && ./onlyweb.sh
+#升级后的密码
+shenz520
